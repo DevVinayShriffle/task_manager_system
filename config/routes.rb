6c8a78bf177c886aaf8f0ae54ccee1ffd9a23315
DevVinayShriffle <vinayks@shriffle.com>
2026-02-11 20:50:34 +0000

    Implemented sidekiq routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  
   root "users#login"
   resource :users, only: [:create, :update, :destroy, :edit] do
     post :login, on: :collection
